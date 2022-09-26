be967ec3577c8b116452f5854fa6e4dc9741ff28
tchen2 <chen.ter@husky.neu.edu>
2022-09-26 19:09:54 -0400

    finished structural elements

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-function revealMC() {
-
-}
-
-function revealColormap() {
-    
+function reveal(e) {
+    button = document.getElementById(e.target.id);
+    switch(e.target.id) {
+        case "MCbutton1":
+            let MCbuttonDiv1 = document.getElementById('MCbutton-id1');
+            button.style.display='none';
+            MCbuttonDiv1.innerHTML = 'marks and channels 1';
+            break;
+        case "MCbutton2":
+            let MCbuttonDiv2 = document.getElementById('MCbutton-id2');
+            button.style.display='none';
+            MCbuttonDiv2.innerHTML = 'marks and channels 2';
+            break;
+        case "Cbutton1":
+            let CbuttonDiv1 = document.getElementById('Cbutton-id1');
+            button.style.display='none';
+            CbuttonDiv1.innerHTML = 'colormap 1';
+            break;
+        case "Cbutton2":
+            let CbuttonDiv2 = document.getElementById('Cbutton-id2');
+            button.style.display='none';
+            CbuttonDiv2.innerHTML = 'colormap 2';
+            break;
+    }
 }
